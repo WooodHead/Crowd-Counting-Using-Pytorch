db43ad6206715da213188815d8e20881f2c65cbb
wooodhead <jcwsw129@gmail.com>
2020-04-05 18:44:53 +0800

    save

--- GenerateGroudnTruth.py
+++ GenerateGroudnTruth.py
@@ -65,19 +65,20 @@ for path in path_sets:
 
 print(img_paths)
 for img_path in img_paths:
-    print ('img_path',img_path)
+    print('img_path',img_path)
     mat = io.loadmat(img_path.replace('.jpg','.mat').replace('images','ground-truth').replace('IMG_','GT_IMG_'))
-    print('mat',mat)
     img= plt.imread(img_path)
     k = np.zeros((img.shape[0],img.shape[1]))
     gt = mat["image_info"][0,0][0,0][0]
+    print('len(gt)',len(gt))
     for i in range(0,len(gt)):
         if int(gt[i][1])<img.shape[0] and int(gt[i][0])<img.shape[1]:
             k[int(gt[i][1]),int(gt[i][0])]=1
     k = gaussian_filter_density(k)
     with h5py.File(img_path.replace('.jpg','.h5').replace('images','ground-truth'), 'w') as hf:
             hf['density'] = k
-# plt.imshow(Image.open(img_paths[0]))
-# gt_file = h5py.File(img_paths[0].replace('.jpg','.h5').replace('images','ground-truth'),'r')
-# groundtruth = np.asarray(gt_file['density'])
-# np.sum(groundtruth)
+
+plt.imshow(Image.open(img_paths[0]))
+gt_file = h5py.File(img_paths[0].replace('.jpg','.h5').replace('images','ground-truth'),'r')
+groundtruth = np.asarray(gt_file['density'])
+np.sum(groundtruth)
